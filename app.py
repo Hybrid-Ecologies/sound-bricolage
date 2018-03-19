9021c2cce119e3f9ac4a6fc0abae67f1f40ec0d6
cooliotonyio <tonyzhao@berkeley.edu>
2018-03-18 21:06:59 -0700

    added sounds and route

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+@app.route("/sounds/<folder>/<sound>")
+def get_sound(folder, sound):
+    return app.send_static_file('sounds/{}/{}.wav'.format(folder, sound))
+
 port = os.getenv('PORT', '8080')
 if __name__ == "__main__":
+    
     app.run(host='0.0.0.0', port=int(port))
