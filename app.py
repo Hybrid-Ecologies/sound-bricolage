1d811c9176b3bcbb058b635c1c09e43a66e1998e
cooliotonyio <tonyzhao@berkeley.edu>
2018-03-19 18:22:48 -0700

    now lists folders

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
+sounds = {}
+for folder in os.listdir("static/sounds"):
+    if folder[0] != "." and folder[-4:] != ".txt":              #don't want to grab hidden files
+        sounds[folder] = [sound for sound in os.listdir("static/sounds/{}".format(folder)) if sound[0] !="."]
 
 @app.route("/")
 def main():
-    return render_template("index.html")
+    return render_template("index.html", sounds = sounds)
 
 @app.route("/sounds/<folder>/<sound>")
 def get_sound(folder, sound):
