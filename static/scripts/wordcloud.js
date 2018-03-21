d0b421d55532aebf6fd9fd5897b36e5d0708952a
cooliotonyio <tonyzhao@berkeley.edu>
2018-03-21 13:54:03 -0700

    prettied up wordcloud

--- static/scripts/wordcloud.js
+++ static/scripts/wordcloud.js
@@ -5,6 +5,10 @@ app.config(['$interpolateProvider', function($interpolateProvider) {
   }]);
 
 app.controller("WordcloudCtrl", function($scope, $window, $element) {
+
+    var max_word_size = screen.width * .05;
+    var min_word_size = max_word_size / 5
+
     $scope.init = function (sounds) {
         $scope.sounds = sounds;
         $scope.folders = Object.keys(sounds);
@@ -18,8 +22,8 @@ app.controller("WordcloudCtrl", function($scope, $window, $element) {
             sound_name = $scope.sounds[folder][i]
             words.push({
                 text: sound_name.slice(0, -4),
-                size: 25,
-                color: '#6d989e',
+                size: min_word_size + (max_word_size - min_word_size) * Math.random(),
+                color: '#'+Math.floor(Math.random()*16777215).toString(16),
                 filename: sound_name
             });
         }
@@ -32,10 +36,10 @@ app.controller("WordcloudCtrl", function($scope, $window, $element) {
     }
 
     $scope.rotate = function(){
-        return ~~(Math.random() * 2) * 90;
+        return ((Math.random() * 2) -1) * 90;
     }
 
     $scope.random = function(){
-        return .4;
+        return .5;
     }
 });
