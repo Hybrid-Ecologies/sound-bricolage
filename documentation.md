32998a5f7ebb8f72f0a49e0f3a2db99ae581070f
cooliotonyio <tony.ytzhao@gmail.com>
2018-04-30 15:19:46 -0700

    documentation

--- documentation.md
+++ documentation.md
@@ -2,12 +2,12 @@
 ___
 
 
-Create a new SoundUI object with your loaded array of selected sounds and default options
+Create a new SoundUI object with your loaded array of selected sounds and default options, binding it to a DOM element
 
 ~~~
 > sounds = [...]
 > options = {...}
-> mySoundUI = new SoundUI(sounds, options)
+> mySoundUI = new SoundUI(sounds, options, elementID)
 ~~~
 
 See sounds available in scope
@@ -24,8 +24,44 @@ Play any specific sound by name
 > mySoundUI.play("sound1")
 ~~~
 
-Bind sound to an element by id
+Bind sound to an event on an element by id, options will override default options
 
 ~~~
-> mySoundUI.bind("sound1","button1")
+> mySoundUI.bind("sound1","button1", options)
+~~~
+
+Removes all bound events from an element
+
+~~~
+>  mySoundUI.unbind("button1")
+~~~
+
+Get a list of all event bindings on an element
+
+~~~
+> mySoundUI.bindings("button1")
+< [{elementID:"button1", options:{...}, sound:"sound1"}, ...]
+~~~
+
+Hide bound element
+
+~~~
+> mySoundUI.hide()
+~~~
+
+Show bound element
+
+~~~
+> mySoundUI.show()
+~~~
+
+Options, `playType` is how sound is played where `eventType` is the triggering event
+
+~~~
+> var options = {
+> 	playType: "oneShotPolyphonic", 
+> 		// Could be "oneShotPolyphonic", "oneShotMonophonic" ,"gate" or "loop"
+> 	eventType: "hover",
+> 		// Could be "hover", "click"
+> }
 ~~~
--- wrapper.js
+++ wrapper.js
@@ -9,26 +9,37 @@ function SoundUI(sounds, options) {
 		playType: "oneShotPolyphonic", // Could be "oneShotPolyphonic" , "oneShotMonophonic" , "gate" or "loop"
 		eventType: "hover",
 	}
-
+	$.extend(defaultOptions, options);
 	this.sounds = sounds;
-	this.options = options;
+	this.options = defaultOptions;
 
 	for (var i = 0; i < sounds.length; i++) {
 		var sound = sounds[i]
 		var audio = new Audio();
 		sound.polyTmpAudio = [];
 		sound.polyTmpAudioKey = 0;
+		if (sound.text != null && sound.name == null) {
+			sound.name = sound.text;
+		}
 		try {
-			if (sound.url == null) {
+			if ((sound.url != null) && (sound.name != null)) {
 				audio.src = sound.url;
 				sound.audio = audio;
 			} else {
-				throw "Runtime Error: Sound needs an url";
+				throw "Runtime Error: Sound needs both an url and a name";
 			};
 		} catch (e) {
 			console.log(e);
 			sound.audio = null;
 		}
+	}
+
+	this.list = function() {
+		var soundNames = [];
+		for (var i = 0; i < this.sounds.length; i++) {
+			soundNames.push(this.sounds[i].name);
+		}
+		return soundNames;
 	}
 
 	this.play = function(soundName) {
@@ -61,16 +72,26 @@ function SoundUI(sounds, options) {
 					break;
 				case 'oneShotPolyphonic' :
 				default:
-				if(sound.url != null){
+				if(sound.url != null) {
 						polyTmpAudio[polyTmpAudioKey] = new Audio(sound.url);
 						polyTmpAudio[polyTmpAudioKey].play();
 						polyTmpAudio[polyTmpAudioKey].addEventListener('ended', function() {
 							polyTmpAudio[polyTmpAudioKey] = null;
 						});
 						polyTmpAudioKey++;
-					}
+				}
 				break;
 			}
 		}
 	}
 }
+
+
+/*
+var sounds = [{"folder":"inputs","text":"Jungle Thunder","filename":"Jungle Thunder.wav","filesize":179596,"url":"http://localhost:8080/sounds/inputs/Jungle Thunder.wav"}]
+var options = {
+	playType: 'gate'
+}
+lib = new SoundUI(sounds, options)
+*/
+
